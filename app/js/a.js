113102bbee88450fd727707381f0cc5ab2e5ba45
developer <beautycode@mail.com>
2017-01-05 15:55:16 +0200

    Created simple Person class to show ES6 functionality.

--- app/js/a.js
+++ app/js/a.js
@@ -1,3 +1,18 @@
 const sum = (a,b) => {
     return a + b;
 };
+
+class Person {
+    
+    constructor(name = "Default", age = 100) {
+        
+        this.name = name;
+        this.age = age;
+        
+    }
+    
+    getInfo() {
+        return `My name is ${this.name}, I am ${this.age}.`;
+    }
+    
+}
--- app/js/b.js
+++ app/js/b.js
@@ -1,1 +1,3 @@
+const person1 = new Person();
 alert( sum(5,6) );
+alert( person1.getInfo() );
